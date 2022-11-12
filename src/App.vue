720276472aa18a3e3ec64c3cc0146d4c0ad5382c
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2022-11-12 16:50:45 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="js">
+<script setup lang="ts">
 //import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
